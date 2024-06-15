78978f53b414d4ec1d0ce0c90b3f1224c8d16b4b
Ismael Parada <ismaelp@kth.se>
2024-06-15 20:26:43 +0200

    cleanup

--- bruteforce_solver.py
+++ bruteforce_solver.py
@@ -1,8 +1,8 @@
 import trait_tools
 
 def main():
-    unit_trait_dict = trait_tools.create_unit_trait_dict()
-    trait_threshold_dict = trait_tools.create_trait_threshold_dict()
+    unit_trait_dict = trait_tools.create_unit_trait_dict("units.csv")
+    trait_threshold_dict = trait_tools.create_trait_threshold_dict("traits.csv")
 
     print(unit_trait_dict)
 
